ef838c1f049a222880b5f5c329be03a164ee7d78
Georgi Petrushev <figoto660@gmail.com>
2019-09-08 18:16:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ function increaseRankBy(n){
   const numInc= document.querySelectorAll('ul.ranked-list li')
 
   for (let i = 0; i < numInc.length; i++){
-    numInc[i].innerHTML = parseInt(numInc[i].innerHTML) + n
+    numInc[i].innerHTML = parseInt(numInc[i].innerHTML) + n;
     }
 }
 
 function deepestChild(){
-  var deep = document.getElementById('grand-node').querySelectorAll('div')
-  return deep[deep.length-1]
+  var deep = document.getElementById('grand-node').querySelectorAll('div');
+  return deep[deep.length-1];
 
 }
