fabb91a517a4dd0f1834ff2dbcfd169d2699c130
Georgi Petrushev <figoto660@gmail.com>
2019-09-08 18:16:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+ return document.querySelector('#nested .target');
+}
+
+
+function increaseRankBy(n){
+  const numInc= document.querySelectorAll('ul.ranked-list li')
+
+  for (let i = 0; i < numInc.length; i++){
+    numInc[i].innerHTML = parseInt(numInc[i].innerHTML) + n;
+    }
+}
+
+function deepestChild(){
+  var deep = document.getElementById('grand-node').querySelectorAll('div');
+  return deep[deep.length-1];
+
+}
